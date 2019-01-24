c9177dd0f22f628e8e94dec67e00fd48c2842c0e
Josephine Reyes <jgreyes@ucsd.edu>
2019-01-23 18:32:19 -0800

    added Jjavascript

---
+++ static/js/introHCI.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    var containingProject = $(this).closest(".project");
+    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+}
+
+function initializePage() {
+	$("#testjs").click(function(e) {
+		$('.jumbotron h1').text("Javascript is connected");
+		$("a.thumbnail").click(projectClick);
+
+	});
+
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
